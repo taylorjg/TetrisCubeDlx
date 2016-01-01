380b52f786e1b8eb1a2f027835ed58b3e55b0613
Jonathan Taylor <jonathan.taylor@dsl.pipex.com>
2016-01-01 18:53:31 +0000

    Checking in renamed file

---
+++ TetrisCubeDlx/Rotation.cs
@@ -0,0 +1,20 @@
+﻿namespace TetrisCubeDlx
+{
+    public enum Rotation
+    {
+        X0Cw,
+        X90Cw,
+        //X180Cw,
+        //X270Cw,
+
+        Y0Cw,
+        //Y90Cw,
+        //Y180Cw,
+        //Y270Cw,
+
+        Z0Cw,
+        Z90Cw,
+        Z180Cw,
+        Z270Cw
+    }
+}
