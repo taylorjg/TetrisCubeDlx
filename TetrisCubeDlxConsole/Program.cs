8fd04444c9e95e675df0ba27da148673ac6f0db4
Jonathan Taylor <jonathan.taylor@dsl.pipex.com>
2016-01-02 13:19:07 +0000

    Added code to render a basic view of the cube

--- TetrisCubeDlxConsole/Program.cs
+++ TetrisCubeDlxConsole/Program.cs
@@ -19,6 +19,8 @@ namespace TetrisCubeDlxConsole
             if (solution != null)
             {
                 DumpSolutionSimple(solution, dlxMatrix);
+                Console.WriteLine();
+                DumpSolutionCube(solution, dlxMatrix);
             }
         }
 
@@ -29,9 +31,40 @@ namespace TetrisCubeDlxConsole
                 var dlxMatrixRow = dlxMatrix[rowIndex];
                 var internalRow = dlxMatrixRow.InternalRow;
                 Console.WriteLine($"Name: {internalRow.Name}; Colour: {internalRow.Colour}");
-                var occupiedSquares = internalRow.OccupiedSquares;
-                Console.WriteLine($"\tOccupied squares: {string.Join(", ", occupiedSquares.Select(c => c.ToString()))}");
             }
+        }
+
+        private static void DumpSolutionCube(Solution solution, IReadOnlyList<DlxMatrixRow> dlxMatrix)
+        {
+            var internalRows = solution.RowIndexes.Select(rowIndex => dlxMatrix[rowIndex].InternalRow).ToList();
+            DumpSolutionCubeHorizontalSlice(internalRows, 3);
+            DumpSolutionCubeHorizontalSlice(internalRows, 2);
+            DumpSolutionCubeHorizontalSlice(internalRows, 1);
+            DumpSolutionCubeHorizontalSlice(internalRows, 0);
+        }
+
+        private static void DumpSolutionCubeHorizontalSlice(IList<InternalRow> internalRows, int y)
+        {
+            for (var z = 3; z >= 0; z--)
+            {
+                var line = "";
+                for (var x = 0; x <= 3; x++)
+                {
+                    var coords = new Coords(x, y, z);
+                    var name = FindPieceNameAt(internalRows, coords);
+                    line += name;
+                }
+                Console.WriteLine(line);
+            }
+
+            Console.WriteLine();
+        }
+
+        private static string FindPieceNameAt(IEnumerable<InternalRow> internalRows, Coords coords)
+        {
+            return internalRows
+                .Single(internalRow => internalRow.OccupiedSquares.Any(occupiedSquare => occupiedSquare.Equals(coords)))
+                .Name;
         }
     }
 }
