7b5675b752c04bc0b5953343cd520d1c9f55a6d8
Jonathan Taylor <jonathan.taylor@dsl.pipex.com>
2016-01-02 10:37:18 +0000

    Started work on determining unique rotations of a piece

---
+++ TetrisCubeDlx/UniqueRotations.cs
@@ -0,0 +1,51 @@
+﻿using System.Collections.Generic;
+using System.Linq;
+
+namespace TetrisCubeDlx
+{
+    public static class UniqueRotations
+    {
+        public static IEnumerable<RotatedPiece> OfPiece(Piece piece)
+        {
+            var setsOfRotations = new[]
+            {
+                new [] {Rotation.X90Cw},
+                new [] {Rotation.X180Cw},
+                new [] {Rotation.X270Cw},
+                new [] {Rotation.Y90Cw},
+                new [] {Rotation.Y180Cw},
+                new [] {Rotation.Y270Cw},
+                new [] {Rotation.X90Cw},
+                new [] {Rotation.X180Cw},
+                new [] {Rotation.X270Cw}
+            };
+
+            return setsOfRotations
+                .Select(rotations => new RotatedPiece(piece, rotations))
+                .Distinct(new RotatedPieceEqualityComparer());
+        }
+
+        private class RotatedPieceEqualityComparer : IEqualityComparer<RotatedPiece>
+        {
+            public bool Equals(RotatedPiece rotatedPiece1, RotatedPiece rotatedPiece2)
+            {
+                var occupiedSquares1 = rotatedPiece1.OccupiedSquares().ToList();
+                var occupiedSquares2 = rotatedPiece2.OccupiedSquares().ToList();
+
+                return
+                    IsEmpty(occupiedSquares1.Except(occupiedSquares2)) &&
+                    IsEmpty(occupiedSquares2.Except(occupiedSquares1));
+            }
+
+            public int GetHashCode(RotatedPiece obj)
+            {
+                return 0;
+            }
+
+            private static bool IsEmpty<T>(IEnumerable<T> source)
+            {
+                return !source.Any();
+            }
+        }
+    }
+}
---
+++ TetrisCubeDlxTests/UniqueRotationsTests.cs
@@ -0,0 +1,31 @@
+﻿using System.Linq;
+using NUnit.Framework;
+using TetrisCubeDlx;
+
+namespace TetrisCubeDlxTests
+{
+    [TestFixture]
+    public class UniqueRotationsTests
+    {
+        [Test]
+        public void UnitCube()
+        {
+            var initStrings = new[]
+            {
+                new[]
+                {
+                    "X"
+                }
+            };
+
+            var piece = new Piece(initStrings);
+            Assert.That(piece.Width, Is.EqualTo(1));
+            Assert.That(piece.Height, Is.EqualTo(1));
+            Assert.That(piece.Depth, Is.EqualTo(1));
+
+            var actual = UniqueRotations.OfPiece(piece);
+
+            Assert.That(actual.Count(), Is.EqualTo(1));
+        }
+    }
+}
