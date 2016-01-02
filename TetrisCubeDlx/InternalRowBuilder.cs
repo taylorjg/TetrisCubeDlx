e891257fed633f6d64123b91ea639257f434bb06
Jonathan Taylor <jonathan.taylor@dsl.pipex.com>
2016-01-02 12:39:41 +0000

    Got a solution!

--- TetrisCubeDlx/InternalRowBuilder.cs
+++ TetrisCubeDlx/InternalRowBuilder.cs
@@ -1,6 +1,44 @@
-﻿namespace TetrisCubeDlx
+﻿using System.Collections.Generic;
+using System.Linq;
+
+namespace TetrisCubeDlx
 {
-    public class InternalRowBuilder
+    public static class InternalRowBuilder
     {
+        public static IEnumerable<InternalRow> BuildInternalRows(IEnumerable<Piece> pieces)
+        {
+            var allLocations = (
+                from x in Enumerable.Range(0, 4)
+                from y in Enumerable.Range(0, 4)
+                from z in Enumerable.Range(0, 4)
+                select new Coords(x, y, z))
+                .ToList();
+
+            return
+                from piece in pieces
+                from rotatedPiece in UniqueRotations.OfPiece(piece)
+                from location in allLocations
+                let placedPiece = new PlacedPiece(rotatedPiece, location)
+                where PlacedPieceIsWithinCube(placedPiece)
+                select new InternalRow(placedPiece);
+        }
+
+        private static bool PlacedPieceIsWithinCube(PlacedPiece placedPiece)
+        {
+            return placedPiece.OccupiedSquares().All(OccupiedSquareIsWithinCube);
+        }
+
+        private static bool OccupiedSquareIsWithinCube(Coords coords)
+        {
+            return
+                DimensionIsWithinCube(coords.X) &&
+                DimensionIsWithinCube(coords.Y) &&
+                DimensionIsWithinCube(coords.Z);
+        }
+
+        private static bool DimensionIsWithinCube(int d)
+        {
+            return d >= 0 && d <= 3;
+        }
     }
 }
--- TetrisCubeDlxConsole/Program.cs
+++ TetrisCubeDlxConsole/Program.cs
@@ -1,9 +1,37 @@
-﻿namespace TetrisCubeDlxConsole
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using DlxLib;
+using TetrisCubeDlx;
+
+namespace TetrisCubeDlxConsole
 {
     internal static class Program
     {
         private static void Main()
         {
+            var pieces = Pieces.MakePieces();
+            var internalRows = InternalRowBuilder.BuildInternalRows(pieces);
+            var dlxMatrix = DlxMatrixBuilder.BuildDlxMatrix(internalRows, pieces).ToList();
+
+            var solution = new Dlx().Solve(dlxMatrix, rows => rows, row => row.Bits).First();
+
+            if (solution != null)
+            {
+                DumpSolutionSimple(solution, dlxMatrix);
+            }
+        }
+
+        private static void DumpSolutionSimple(Solution solution, IReadOnlyList<DlxMatrixRow> dlxMatrix)
+        {
+            foreach (var rowIndex in solution.RowIndexes)
+            {
+                var dlxMatrixRow = dlxMatrix[rowIndex];
+                var internalRow = dlxMatrixRow.InternalRow;
+                Console.WriteLine($"Name: {internalRow.Name}; Colour: {internalRow.Colour}");
+                var occupiedSquares = internalRow.OccupiedSquares;
+                Console.WriteLine($"\tOccupied squares: {string.Join(", ", occupiedSquares.Select(c => c.ToString()))}");
+            }
         }
     }
 }
